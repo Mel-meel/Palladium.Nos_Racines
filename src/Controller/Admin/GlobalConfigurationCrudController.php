5bae12752a75da3a626b78cfdfcd355713775dc9
Mel-meel <mel.avizou@laposte.net>
2024-12-23 10:46:02 +0100

    Translations gestion. Adding a global configuration system.

---
+++ src/Controller/Admin/GlobalConfigurationCrudController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\GlobalConfigurations;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class GlobalConfigurationCrudController extends AbstractCrudController
+{
+    private TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public static function getEntityFqcn(): string
+    {
+        return GlobalConfigurations::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('globalClanName', $this->translator->trans('global_clan_name')),
+            BooleanField::new('maintenanceMode', $this->translator->trans('maintenance_mode')),
+            BooleanField::new('activeConnectionLinkStartPage', $this->translator->trans('active_connection_link_start_page')),
+            BooleanField::new('activeReceptionTemplate', $this->translator->trans('active_reception_template')),
+            ChoiceField::new('globalDefaultLanguage', $this->translator->trans('global_default_language'))
+                ->setChoices([
+                    $this->translator->trans('Français') => 'fr',
+                    $this->translator->trans('Anglais') => 'en',
+                    $this->translator->trans('Espagnol') => 'es',
+                ]),
+            BooleanField::new('multilanguageFamily', $this->translator->trans('multilanguage_family')),
+            TextField::new('customFooterText', $this->translator->trans('custom_footer_text'))->hideOnIndex(),
+            IntegerField::new('customAdvancedMainMenu', $this->translator->trans('custom_advanced_main_menu')),
+            IntegerField::new('customAdvancedMainMenuMobile', $this->translator->trans('custom_advanced_main_menu_mobile')),
+        ];
+    }
+}
--- src/Controller/DebugController.php
+++ src/Controller/DebugController.php
@@ -1,22 +1,20 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller ;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController ;
+use Symfony\Component\HttpFoundation\Response ;
+use Symfony\Component\Routing\Annotation\Route ;
 
-class DebugController extends AbstractController
-{
+class DebugController extends AbstractController {
     #[Route('/debug/roles', name: 'debug_roles')]
-    public function debugRoles(): Response
-    {
-        $user = $this->getUser();
+    public function debugRoles() : Response {
+        $user = $this->getUser() ;
 
         if (!$user) {
-            return new Response('No user is logged in.');
+            return new Response('No user is logged in.') ;
         }
 
-        return new Response('Roles: ' . implode(', ', $user->getRoles()));
+        return new Response('Roles: ' . implode(', ', $user->getRoles())) ;
     }
 }
